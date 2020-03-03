3dc70dd8fca4cf0b2d9fe72f6ece9763e5ca1536
Francis Sualog <fja0377@gmail.com>
2020-03-02 23:36:46 -0800

    Celsius to Fahrenheit

---
+++ 1st.py
@@ -0,0 +1,4 @@
+hours = input("Enter Number of Hours:")
+rate = input("Enter Number of Rate:")
+pay = float(hours) * float(rate)
+print(pay)
---
+++ act.py
@@ -0,0 +1,3 @@
+celsius = input("Enter Celsius Temperature")
+fahrenheit = (float(celsius) * 9/5) + 32
+print(fahrenheit)
